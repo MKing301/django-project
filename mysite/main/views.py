bcd062362f0ec123380a38cb899927dd90ad956f
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-09 21:23:26 -0500

    Add view for main app register page

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request=request,
                   template_name="main/home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:home")
+        else:
+            # TODO display error messages (just printing for now)
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm
+    return render(request=request,
+                  template_name="main/register.html",
+                  context={"form":form})
