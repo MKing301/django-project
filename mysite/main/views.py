3d9d9d286481c47e2c6484cae955eaa2887fc359
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-10 21:12:04 -0500

    Add messages to register view of main app

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,8 +5,7 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request=request,
-                  template_name="main/home.html")
+    return render(request=request, template_name="main/home.html")
 
 
 def register(request):
@@ -14,14 +13,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"New account created: {username}")
             login(request, user)
+            messages.success(request, f"You are now logged in as {username}")
             return redirect("main:home")
         else:
-            # TODO display error messages (just printing for now)
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
     form = UserCreationForm
-    return render(request=request,
-                  template_name="main/register.html",
-                  context={"form":form})
+    return render(request=request, template_name="main/register.html", context={"form": form})
