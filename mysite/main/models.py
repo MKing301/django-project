6a8c37757f7d84cb43cffc5d8f80728b788077f1
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-12 17:16:06 -0500

    Add books model to main app

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
