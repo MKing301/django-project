91787c3b031dd64286d7590b32887e09ff0e1050
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-14 14:35:53 -0400

    Add edit profile form to main app

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
@@ -12,6 +12,23 @@ class NewUserForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            return user
+
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", 'password', 'email')
+
+    def save(self, commit=True):
+        user = super(EditProfileForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.password = self.cleaned_data['password']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
