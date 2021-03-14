b92b3083e5133154ca0979e8c4603e2d0362b141
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-13 21:13:27 -0500

    Register book and book category models to admin

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BookCategory, Book
+from django.db import models
+
 
 # Register your models here.
+admin.site.register(BookCategory)
+admin.site.register(Book)
