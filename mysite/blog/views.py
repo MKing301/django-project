d1b692a837608b17b0810af4647ca8b3187458aa
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-24 21:07:55 -0400

    Add post detail view to blog app

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CommentForm
 from .models import Post
 
 # Create your views here.
@@ -8,3 +9,24 @@ def blog(request):
     return render(request=request,
                   template_name='blog/home.html',
                   context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('blog:post_detail', slug=post.slug)
+
+    else:
+        form = CommentForm()
+
+    return render(request=request,
+                  template_name='blog/post_detail.html',
+                  context={'post': post, 'form': form})
