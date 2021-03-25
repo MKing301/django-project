5bf512d8cc0b5999c04a1556fc69a35e9c6a6da2
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-03-24 21:06:30 -0400

    Add comment model to blog app

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -10,3 +11,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-inserted_date']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.ForeignKey(User, related_name='usernames', on_delete=models.CASCADE)
+    body = models.TextField()
+    comment_inserted_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['comment_inserted_date']
