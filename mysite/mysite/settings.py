43f5e63e3d314f0c3a9f3353efbde1488ad5f9eb
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-04-14 20:17:34 -0400

    Chg settings email backend from filebased to smtp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,8 +29,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # File backend
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+# SMTP Configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
